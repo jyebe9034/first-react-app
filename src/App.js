89a6d5d6458c02ba9d80a146abb786beb51cc9e2
jyebe9034 <jyebe9034@gmail.com>
2022-01-03 22:16:10 +0900

    learning about component and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.subTitle}
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" subTitle="world wide web!"></Subject>
+        <Subject title="React" subTitle="For UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
