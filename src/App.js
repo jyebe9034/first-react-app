9bad22b56700ef0f6dc609d5b18bae9fdf432390
jyebe9034 <jyebe9034@gmail.com>
2022-01-07 22:28:09 +0900

    learn state2

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      subject : {
+      subject: {
         title: 'WEB',
         subTitle: 'world wide web'
-      }
+      },
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
     }
   }
   render() {
     return (
       <div className="App">
         <Subject 
-          title={this.state.subTitle.title} 
-          subTitle={this.state.subTitle.subTitle}>
+          title={this.state.subject.title} 
+          subTitle={this.state.subject.subTitle}>
         </Subject>
         <Subject title="React" subTitle="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,17 +1,22 @@
 import { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      return (
-        <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-          </ul>
-        </nav>
-      );
+  render() {
+    const list = []
+    const data = this.props.data
+    let i = 0
+    while (i < data.length) {
+      list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+      i = i + 1
     }
+    return (
+      <nav>
+        <ul>
+          {list}
+        </ul>
+      </nav>
+    );
+  }
   }
 
 export default TOC;
