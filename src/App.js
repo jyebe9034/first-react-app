c0ebd615043a15613537bbf9d978eae4c4779ac9
jyebe9034 <jyebe9034@gmail.com>
2022-01-07 22:13:27 +0900

    learn state1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      subject : {
+        title: 'WEB',
+        subTitle: 'world wide web'
+      }
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" subTitle="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subTitle.title} 
+          subTitle={this.state.subTitle.subTitle}>
+        </Subject>
         <Subject title="React" subTitle="For UI"></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
