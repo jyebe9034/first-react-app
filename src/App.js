7200d3d76f2e1313afba6b41fd3e9a284e04ad71
jyebe9034 <jyebe9034@gmail.com>
2022-01-09 22:17:23 +0900

    state and render()

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      mode: 'welcome',
       subject: {
         title: 'WEB',
         subTitle: 'world wide web'
+      },
+      welcome: {
+        title: 'Welcome',
+        desc: 'Hello, React!!' 
       },
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information'},
@@ -20,15 +25,22 @@ class App extends Component {
     }
   }
   render() {
+    let _title, _desc = null
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
           subTitle={this.state.subject.subTitle}>
         </Subject>
-        <Subject title="React" subTitle="For UI"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
